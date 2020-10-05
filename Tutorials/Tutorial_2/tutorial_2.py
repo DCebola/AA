2b1c5decba3ff2140f0da4698c275fea105d5a81
Diogo Cebola <d.cebola@campus.fct.unl.pt>
2020-10-05 17:13:45 +0100

    exercise 2.2 done

--- Tutorials/Tutorial_2/tutorial_2.py
+++ Tutorials/Tutorial_2/tutorial_2.py
@@ -2,51 +2,72 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def random_split(data, test_points):
+def random_split(_data, test_points):
     """return two matrices splitting the data at random3"""
-    ranks = np.arange(data.shape[0])
+    ranks = np.arange(_data.shape[0])
     np.random.shuffle(ranks)
-    train = data[ranks >= test_points, :]
-    test = data[ranks < test_points, :]
-    return train, test
+    _train = _data[ranks >= test_points, :]
+    _test = _data[ranks < test_points, :]
+    return _train, _test
 
 
-def polyFit(degree, x, y, color):
-    coefs = np.polyfit(x, y, degree)
-    pxs = np.linspace(0, max(x), 100)
-    poly = np.polyval(coefs, pxs)
-    plt.plot(pxs, poly, color)
+def polyFit(_degree, _x, _y, color, _pxs):
+    _coefs = np.polyfit(_x, _y, _degree)
+    poly = np.polyval(_coefs, _pxs)
+    plt.plot(_pxs, poly, color)
+    return _coefs
+
+
+def mean_square_error(_data, _coefs):
+    pred = np.polyval(_coefs, _data[:, 0])
+    error = np.mean((_data[:, 1] - pred) ** 2)
+    return error
 
 
 np.set_printoptions(precision=2)
-data = np.loadtxt('bluegills.txt', delimiter='\t', skiprows=1)
+data = np.loadtxt('yield.txt', delimiter='\t', skiprows=1)
+
 Ys = data[:, 1]
 Xs = data[:, 0]
 
-means = np.mean(Xs)
-stdevs = np.std(Xs)
-Xs = (Xs - means) / stdevs
-means = np.mean(Ys)
-stdevs = np.std(Ys)
-Ys = (Ys - means) / stdevs
+x_means = np.mean(Xs)
+x_stdevs = np.std(Xs)
+Xs = (Xs - x_means) / x_stdevs
+
+y_means = np.mean(Ys)
+y_stdevs = np.std(Ys)
+Ys = (Ys - y_means) / y_stdevs
 
 data = np.array((Xs, Ys)).T
-# scale = np.max(data,axis=0)
-# data = data/scale  scale then expand check page 28 of lecture notes
-train, temp = random_split(data, 39)
-valid, test = random_split(temp, 19)
 
+data_size = data.shape[0]
+train, temp = random_split(data, data_size / 2)
+valid, test = random_split(temp, data_size / 4)
+
+pxs = np.linspace(min(data[:, 0]), max(data[:, 0]), 100)
 x, y = (train[:, 0], train[:, 1])
-plt.plot(x, y, '+r')
+plt.plot(x, y, '.r')
 plt.plot(valid[:, 0], valid[:, 0], 'sg')
 plt.plot(test[:, 0], test[:, 0], '^b')
 plt.figure(1, figsize=(12, 8), frameon=False)
-plt.axis([-3, 3, -4, 2])
+plt.axis()
 plt.title("Blue gill size")
 
 colors = ['-b', '-r', '-g', '-c', '-y', '-m']
+best_error = 10000000
+best_coefs = []
+best_degree = -1
 for degree in range(1, 7):
-    polyFit(degree, x, y, colors[degree - 1])
+    coefs = polyFit(degree, x, y, colors[degree - 1], pxs)
+    valid_error = mean_square_error(valid, coefs)
+    if valid_error < best_error:
+        best_error = valid_error
+        best_coefs = coefs
+        best_degree = degree
+
+test_error = mean_square_error(test, best_coefs)
+print("Degree: " + str(best_degree))
+print("Test error: " + str(test_error))
 plt.show()
 
 # train, temp = random_split(data, )
