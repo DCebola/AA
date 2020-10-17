5034ca7f495feb3d492e8581584efc8d4971113c
Diogo Cebola <diogohecebola@gmail.com>
2020-10-17 19:17:05 +0100

    need to calculate error of h

--- Project_1/TP1.py
+++ Project_1/TP1.py
@@ -1,4 +1,4 @@
-#TODO: Three Classifiers:
+# TODO: Three Classifiers:
 # Logistic Regression
 # find the best value for the regularization parameter C
 # C is in [1e-2, 1e12] each step is *10 (1e-2 ,1e-1 ,1e0,...)
@@ -23,13 +23,15 @@ import numpy as np
 from sklearn.utils import shuffle
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold
+from sklearn.neighbors import KernelDensity
+from sklearn.naive_bayes import GaussianNB
 
 FOLDS = 5
-KDE_STEP = 0.02
 MAX_C_EXPONENT = 12
 MIN_C_EXPONENT = -2
-MIN_KDE = 0.02
-MAX_KDE = 0.6
+KDE_STEP = 2
+MIN_KDE = 2
+MAX_KDE = 60
 FEATS = 4
 
 
@@ -45,32 +47,50 @@ def standardize(_train_data, _test_data):
     return np.column_stack((_train_features, _train_classes)), np.column_stack((_test_features, _test_classes))
 
 
-def calc_fold(X, Y, train_ix, valid_ix, C):
-    reg = LogisticRegression(C=C, tol=1e-10)
-    reg.fit(X[train_ix, :FEATS], Y[train_ix])
-    prob = reg.predict_proba(X[:, :FEATS])[:, 1]
-    squares = (prob - Y) ** 2
+def calc_fold_logistic(x, y, train_ix, valid_ix, _c):
+    reg = LogisticRegression(C=_c, tol=1e-10)
+    reg.fit(x[train_ix, :FEATS], y[train_ix])
+    squares = (reg.predict_proba(x[:, :FEATS])[:, 1] - y) ** 2
     return np.mean(squares[train_ix]), np.mean(squares[valid_ix])
 
 
-def logistic_regression(_train_data, ):
+def logistic_regression(_train_data, kf):
     best_val_err = 100000000
-    kf = StratifiedKFold(n_splits=FOLDS)
     best_exp = -3
     for exp in range(MIN_C_EXPONENT, MAX_C_EXPONENT + 1):
         tr_err = va_err = 0
         for tr_ix, va_ix in kf.split(_train_data[:, 4], _train_data[:, 4]):
-            fold_t_err, fold_v_err = calc_fold(_train_data[:, 0:4], _train_data[:, 4], tr_ix, va_ix, 10 ** exp)
+            fold_t_err, fold_v_err = calc_fold_logistic(_train_data[:, 0:4], _train_data[:, 4], tr_ix, va_ix, 10 ** exp)
             tr_err += fold_t_err
             va_err += fold_v_err
         if va_err / FOLDS < best_val_err:
             best_val_err = va_err / FOLDS
             best_exp = exp
-    return 10**best_exp
+    return 10 ** best_exp
 
 
-def custom_naive_bayes(kde, _train_data):
-    pass
+def calc_fold_bayes(x, y, train_ix, valid_ix, __h):
+    kde = KernelDensity(bandwidth=__h)
+    kde.fit(x[train_ix, :FEATS])
+    log = kde.score_samples(np.linspace())
+    print(log)
+    return np.mean(squares[train_ix]), np.mean(squares[valid_ix])
+
+
+def custom_naive_bayes(_train_data, kf):
+    best_val_err = 100000000
+    best_h = -1
+    h_values = map(lambda x: x / 100.0, range(MIN_KDE, MAX_KDE, KDE_STEP))
+    for _h in h_values:
+        tr_err = va_err = 0
+        for tr_ix, va_ix in kf.split(_train_data[:, 4], _train_data[:, 4]):
+            fold_t_err, fold_v_err = calc_fold_bayes(_train_data[:, 0:4], _train_data[:, 4], tr_ix, va_ix, _h)
+            tr_err += fold_t_err
+            va_err += fold_v_err
+        if va_err / FOLDS < best_val_err:
+            best_val_err = va_err / FOLDS
+            best_h = _h
+    return best_h
 
 
 np.set_printoptions(precision=4)
@@ -78,4 +98,5 @@ test_data = np.loadtxt('TP1_test.tsv')
 train_data = np.loadtxt('TP1_train.tsv')
 train_data, test_data = standardize(shuffle(train_data), shuffle(test_data))
 
-c = logistic_regression(train_data)
+c = logistic_regression(train_data, StratifiedKFold(n_splits=FOLDS))
+h = custom_naive_bayes(train_data, StratifiedKFold(n_splits=FOLDS))
