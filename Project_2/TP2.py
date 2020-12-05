209959466258a169a5cf3639ca677bf789b0859a
Kebab <mewseven7777777@gmail.com>
2020-12-05 18:14:54 +0000

    feature f test

--- Project_2/TP2.py
+++ Project_2/TP2.py
@@ -2,32 +2,50 @@ import tp2_aux as utils
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 from sklearn.manifold import Isomap
+from sklearn.feature_selection import f_classif
+from sklearn.cluster import KMeans
+
 import numpy as np
+import pickle
 import pandas as pd
 from functools import partial
 from tqdm import tqdm
+from os import path
 
+
+def normalize(_feats):
+
+
+def f_test(_feats, _labels):
+    _f, probs = f_classif(_feats, _labels)
+    return _f
+
+
+np.set_printoptions(precision=4, suppress=True)
 tqdm = partial(tqdm, position=0, leave=True)
-pca = PCA(n_components=6)
-tsne = TSNE(n_components=6, method='exact')
-iso = Isomap(n_components=6)
+labels = np.loadtxt("labels.txt", delimiter=",")
+if path.exists("feats.p"):
+    f = open("feats.p", "rb")
+    feats = pickle.load(f)
+else:
+    pca = PCA(n_components=6)
+    tsne = TSNE(n_components=6, method='exact')
+    iso = Isomap(n_components=6)
+    print("Extracting features...")
+    img_mat = utils.images_as_matrix()
+    feats = np.column_stack([pca.fit_transform(img_mat), tsne.fit_transform(img_mat), iso.fit_transform(img_mat)])
+    pickle.dump(feats, open("feats.p", "wb"))
 
-# Convert images to panda
-img_mat = utils.images_as_matrix()
-# Extract features
-print("Extracting features...")
-feats_map_keys = ["PCA-1", "PCA-2", "PCA-3", "PCA-4", "PCA-5", "PCA-6",
-                  "TSNE-1", "TSNE-2", "TSNE-3", "TSNE-4", "TSNE-5", "TSNE-6",
-                  "ISO-1", "ISO-2", "ISO-3", "ISO-4", "ISO-5", "ISO-6"]
-feats = np.column_stack([pca.fit_transform(img_mat), tsne.fit_transform(img_mat), iso.fit_transform(img_mat)])
-count = 0
-feats_map = {}
-for key in feats_map_keys:
-    feats_map[key] = feats[:, count]
-    count += 1
-print("Features extracted.")
-print(feats_map["PCA-1"].shape)
+print("Features extracted")
+
+
+f_scores = f_test(feats[np.where(labels[:, -1] != 0)], labels[np.where(labels[:, -1] != 0)][:, -1])
+print(f_scores)
+
 # Create clusters
+
+KMeans()
+
 
 
 # Plot clusters to features
